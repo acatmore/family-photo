f67037dff205e92b1f230565b1b2b79a4dee6e14
Alex Atmore <acatmore@gmail.com>
2018-01-20 13:46:40 -0800

    modal/buttons working

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, Modal, Button, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    modalVisible: false,
+  };
+
+  openModal() {
+    this.setState({modalVisible:true});
+  }
+  
+  closeModal() {
+    this.setState({modalVisible:false});
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Welcome to Family Photo</Text>
         <Text style={styles.text}> Take a picture!</Text>
         {/* credit later taken from https://www.flaticon.com/free-icon/photo-camera_3901#term=camera&page=1&position=8 */}
-        <Image style={styles.icon} source={require('./photo-camera.png')} />
+        
+        <Modal
+              visible={this.state.modalVisible}
+              animationType={'slide'}
+              onRequestClose={() => this.closeModal()}
+          >
+            <View style={styles.cameraContainer}>
+              <View style={styles.innerContainer}>
+                <Text>This is content inside of modal component</Text>
+                <Button
+                    onPress={() => this.closeModal()}
+                    title="Close camera"
+                >
+                </Button>
+              </View>
+            </View>
+          </Modal>
+
+          <TouchableOpacity onPress={() => this.openModal()}>
+            <Image style={styles.icon} source={require('./photo-camera.png')} />
+          </TouchableOpacity>
+
         <Text style={styles.text}> View existing pictures</Text>
-        {/* <View style={styles.container}>
-          <Text style={styles.container}>My Family</Text>
-        </View> */}
       </View>
     );
   }
@@ -32,9 +61,18 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 10,
+    padding: 20,
   },
   icon: {
     width: 50,
     height: 50,
+  },
+  cameraContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+  },
+  innerContainer: {
+    alignItems: 'center',
   },
 });
